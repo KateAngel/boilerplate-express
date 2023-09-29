f76aa607d22690d82ea19784a96817a988d85a96
Kateryna <kvkyrpa@gmail.com>
2023-09-29 19:40:31 +0200

    serve json on a specific route

--- myApp.js
+++ myApp.js
@@ -10,7 +10,9 @@ let app = express();
 
     app.use("/public", express.static(__dirname + "/public"));
 
-
+    app.get('/json', function (req, res) {
+        res.json({"message": "Hello json"})
+    })
 
 
 
