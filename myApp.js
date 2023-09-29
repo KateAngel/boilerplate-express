2735e8973e8e613c615708a9d12866f154b03c10
Kateryna <kvkyrpa@gmail.com>
2023-09-29 20:37:11 +0200

    use body-parser

--- myApp.js
+++ myApp.js
@@ -1,55 +1,58 @@
 require('dotenv').config()
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
-    app.use(function middleware(req, res, next) {
-        console.log(req.method + " " + req.path + " - " + req.ip)
-        next()
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function middleware(req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip)
+    next()
+});
+
+console.log('Hello World!');
+app.get('/', function (req, res) {
+    absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get('/json', function (req, res) {
+    if (process.env.MESSAGE_STYLE==="uppercase") {
+        res.json({"message": "HELLO JSON"})
+    } else {
+        res.json({"message": "Hello json"})
+    }
+})
+
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.send({time: req.time})
+});
+
+app.get("/:word/echo", function(req, res) {
+    const { word } = req.params;
+    res.json({
+    echo: word
     });
+})
 
-    console.log('Hello World!');
-    app.get('/', function (req, res) {
-        absolutePath = __dirname + '/views/index.html';
-        res.sendFile(absolutePath);
+app.route("/name").get( (req, res) => { 
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    res.json({ 
+        name: `${firstName} ${lastName}`
     });
-
-    app.use("/public", express.static(__dirname + "/public"));
-
-    app.get('/json', function (req, res) {
-        if (process.env.MESSAGE_STYLE==="uppercase") {
-            res.json({"message": "HELLO JSON"})
-        } else {
-            res.json({"message": "Hello json"})
-        }
-    })
-
-    app.get('/now', function(req, res, next) {
-        req.time = new Date().toString();
-        next();
-    }, function(req, res) {
-        res.send({time: req.time})
+}).post( (req, res) => { 
+    var firstName = req.bodyy.first;
+    var lastName = req.body.last;
+    res.json({ 
+        name: `${firstName} ${lastName}`
     });
-
-    app.get("/:word/echo", function(req, res) {
-        const { word } = req.params;
-        res.json({
-        echo: word
-        });
-    })
-
-    app.route("/name").get( (req, res) => { 
-        var firstName = req.query.first;
-        var lastName = req.query.last;
-        res.json({ 
-            name: `${firstName} ${lastName}`
-        });
-    }).post( (req, res) => { 
-        var firstName = req.bodyy.first;
-        var lastName = req.body.last;
-        res.json({ 
-            name: `${firstName} ${lastName}`
-        });
-    });
+});
 
 
 
