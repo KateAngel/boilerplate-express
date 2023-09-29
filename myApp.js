ad786b2f25ac150a5972122d048abbe1856ba9e1
Kateryna <kvkyrpa@gmail.com>
2023-09-29 20:10:38 +0200

    chain middleware to create a time server

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ let app = express();
         console.log(req.method + " " + req.path + " - " + req.ip)
         next()
     });
-    
+
     console.log('Hello World!');
     app.get('/', function (req, res) {
         absolutePath = __dirname + '/views/index.html';
@@ -23,7 +23,12 @@ let app = express();
         }
     })
 
-
+    app.get('/now', function(req, res, next) {
+        req.time = new Date().toString();
+        next();
+    }, function(req, res) {
+        res.send({time: req.time})
+    });
 
 
 
